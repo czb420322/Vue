85a83325571a9a961dd9a2d9c09d582add6798dd
czb <2751470984@qq.com>
2021-09-20 22:25:15 +0800

    123

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-15 23:16:38
+ * @LastEditTime: 2021-09-20 22:17:33
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vuetest\src\views\About.vue
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page1223121</h1>
   </div>
 </template>
