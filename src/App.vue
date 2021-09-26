7107bf79e06c476591d80e1a30e24ad9c353cf69
czb <2751470984@qq.com>
2021-09-26 23:30:00 +0800

    第一次提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-15 23:16:38
+ * @LastEditTime: 2021-09-25 15:56:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edrouter
+ * @FilePath: \vuetest\src\App.vue
+-->
 <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/parent">Parent</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
---
+++ src/views/Parent.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-09-25 15:30:08
+ * @LastEditTime: 2021-09-25 15:54:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vuetest\src\views\Parent.vue
+-->
+<template>
+    <div>
+        我是父组件
+    </div>
+</template>
+
+<script setup >
+
+</script>
+
+<style scoped>
+
+</style>
