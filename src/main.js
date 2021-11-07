0431244b1ef801f9db5ec5eef78fa302bdcc3e94
czb <2751470984@qq.com>
2021-11-07 20:42:53 +0800

    引入时间的插件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from './store'
 import Vue2OrgTree from 'vue2-org-tree'
 import 'vue2-org-tree/dist/style.css'
 
+
+/* 引入时间的插件laydate组件库 */
+import 'jquery'
+import './common/laydate/laydate.js'
+import './common/js/layui/layui.all.js'
+import './common/js/layui/css/layui.css'
+import './common/js/layui/css/modules/laydate/default/laydate.css'
+import './common/js/layui/css/modules/layer/default/layer.css'
 Vue.use(Vue2OrgTree)
 Vue.config.productionTip = false
 
