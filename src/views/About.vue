cb8efa58ed50c06979de9114a88c182a6e3f7dc2
czb <2751470984@qq.com>
2021-12-21 14:21:30 +0800

    删除无用的文字说明

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
     <div><router-link to="/home/downTime">开启倒计时</router-link></div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <el-button></el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+let person = {name:"张三",age:25,address:"深圳",getName:function(){}}
+Object.keys(person).map((key)=>{
+return  person[key] // 获取到属性对应的值，做一些处理
+}) 
 export default {
   name: 'Home',
   components: {
     // HelloWorld
-  }
+  },
+  
 }
 </script>
